523b8b64ba7dd0aabdaecd4b9e24cac46df679ab
Priyanshu <priyanshu24003@gmail.com>
2026-02-20 17:49:59 +0530

    CI Check3

--- _test.py
+++ _test.py
@@ -3,11 +3,12 @@ import pytest
 #importing pipeline components
 
 import src.A_data_injestion as A
+import src.helper as H
 
-with open('data/logs/data_ingestion.log') as file:
+# with open('data/logs/data_ingestion.log') as file:
     
-    r = file.readlines()[-6:]
-    print(r)
+#     r = file.readlines()[-6:]
+#     print(r)
 
 print()
 print("THIS IS INSIDE _test.py")
