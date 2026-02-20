107122687a17662d0bf54402b874d785e3fcd98f
Priyanshu <priyanshu24003@gmail.com>
2026-02-20 18:18:25 +0530

    Data Injestion Check 1 CI

--- src/_test.py
+++ src/_test.py
@@ -5,11 +5,17 @@ import pytest
 import A_data_injestion as A
 import helper as H
 
-# with open('data/logs/data_ingestion.log') as file:
-    
-#     r = file.readlines()[-6:]
-#     print(r)
+# def test_check():
+#     assert sum([1,2, 3]) == 6, 'just a silly test'
+#     pass
 
-def test_check():
-    assert sum([1,2, 3]) == 6, 'just a silly test'
-    pass
+def test_A():
+    try:        
+        with open('data/logs/data_ingestion.log') as file:
+            
+            r = file.readlines()[-6:]
+            allone = ''.join(r)
+            if 'ERROR' in allone:
+                raise "ERROR IN DATA_INJESTION"
+    except Exception as e:
+        raise e
