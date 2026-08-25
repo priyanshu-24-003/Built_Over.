d0a8c477bb733c33702818f559701b314de24c42
Priyanshu <priyanshu24003@gmail.com>
2026-08-25 16:53:05 +0530

    clean 2

--- src/_test.py
+++ src/_test.py
@@ -1,14 +1,5 @@
 import pytest
 
-#importing pipeline components
-
-import A_data_injestion as A
-import helper as H
-
-# def test_check():
-#     assert sum([1,2, 3]) == 6, 'just a silly test'
-#     pass
-
 def Log_check(filename, recentlogN):
     with open(filename) as file:
         
